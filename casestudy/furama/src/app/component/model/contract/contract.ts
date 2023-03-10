634927511751e8d4b896ed2599da39b7a85348c4
hau8477 <hau8477@gmail.com>
2023-03-10 17:05:09 +0700

    casestudy

---
+++ casestudy/furama/src/app/component/model/contract/contract.ts
@@ -0,0 +1,15 @@
+export interface Contract {
+
+}
+
+//
+// private Long id;
+// private Customer customer;
+// private Employee employee;
+// private Facility facility;
+// private String startDate;
+// private String endDate;
+// private double deposit;
+// private List<ContractDetail> contractDetail;
+// private boolean flag = true;
+// private double totalCost;
